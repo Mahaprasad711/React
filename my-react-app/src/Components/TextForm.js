c2a5405ca1268adeb1680404338d9c4051b6542b
specGuyWhoCodes <53210746+Mahaprasad711@users.noreply.github.com>
2024-07-05 08:00:03 +0530

    Added text summary functionality
    it counts number of words and characters using hook state

--- my-react-app/src/Components/TextForm.js
+++ my-react-app/src/Components/TextForm.js
@@ -4,20 +4,14 @@ export default function TextForm() {
 
 
   function rewriteSentence(sentence) {
-    // Trim any extra whitespace from the beginning and end of the sentence
     sentence = sentence.trim();
   
-    // Split the sentence into words
     let words = sentence.split(' ');
   
-    // Capitalize the first word and convert the rest to lowercase
     let rewrittenSentence = words.map((word, index) => {
-      // Capitalize the first word
       if (index === 0) {
         return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
       } else {
-        // Check if the previous word ended with a full stop (.)
-        // and capitalize the current word accordingly
         let prevWord = words[index - 1];
         if (prevWord.charAt(prevWord.length - 1) === '.') {
           return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
@@ -69,8 +63,8 @@ export default function TextForm() {
   
   return (
     
-    <div>
-      
+    
+    <> 
   <div className="mb-3">
   <label htmlFor="exampleFormControlTextarea1" className="form-label">Write your message</label>
   <textarea onChange={change} className="form-control" value={yourtext} id="exampleFormControlTextarea1" rows="5" placeholder="message will be displayed here"></textarea>
@@ -78,11 +72,14 @@ export default function TextForm() {
   <button className='btn btn-primary' style={{ marginRight: '10px'}} onClick={convertUpperCase}>Convert to Uppercase <br/></button>
   <button className='btn btn-primary' style={{ marginRight: '10px'}} onClick={convertLowerCase}>Convert to Lowercase</button>
   <button className='btn btn-primary' onClick={sentence1}>Convert to Normal sentence Format</button>
-
-  
-
-  </div>
   </div>
     </div>
+
+<div className="container my-3">
+<h1>Text summary </h1>
+<p> {yourtext.split(" ").length} and {yourtext.length} charecters</p>
+</div>
+</> 
+
   )
 }
